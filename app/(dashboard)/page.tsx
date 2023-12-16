f8f73aa970a1d9135c6aac72d729de898776ffb3
kartikkpawar <knpwar789@gmail.com>
2023-12-17 00:28:16 +0530

    Home Page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,21 +1,110 @@
-import Logo from "@/components/Logo";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-import { UserButton } from "@clerk/nextjs";
-import React, { ReactNode } from "react";
+import React, { ReactNode, Suspense } from "react";
+import { GetFormStats } from "../actions/form";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
+import CreateFormButton from "@/components/CreateFormButton";
 
-function Layout({ children }: { children: ReactNode }) {
+export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex justify-between border-b border-border h-[60px] px-4 py-2 items-center">
-        <Logo />
-        <div className="flex gap-4">
-          <ThemeSwitcher />
-          <UserButton afterSignOutUrl="/sign-in" />
-        </div>
-      </nav>
-      <main className="flex w-full flex-grow">{children}</main>
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <Separator className="my-6" />
+      <CreateFormButton />
     </div>
   );
 }
 
-export default Layout;
+async function CardStatsWrapper() {
+  const stats = await GetFormStats();
+  return <StatsCards loading={false} data={stats} />;
+}
+
+interface StatCardProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards(props: StatCardProps) {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total Visits"
+        icon={<LuView className="text-blue-600" />}
+        helperText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-blue-600"
+      />{" "}
+      <StatsCard
+        title="Total Submissions"
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />{" "}
+      <StatsCard
+        title="Submission Rates"
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submissions"
+        value={data?.visits.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />{" "}
+      <StatsCard
+        title="Bounce Rate"
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leave without interacting with form"
+        value={data?.visits.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-red-600"
+      />
+    </div>
+  );
+}
+
+function StatsCard({
+  title,
+  icon,
+  helperText,
+  value,
+  loading,
+  className,
+}: {
+  title: string;
+  icon: ReactNode;
+  helperText: string;
+  value: string;
+  loading: boolean;
+  className: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm text-muted-foreground">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
+}
