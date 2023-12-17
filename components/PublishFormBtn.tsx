a623702ed00d24129758fd93e458d0245d2fac0e
kartikkpawar <knpwar789@gmail.com>
2023-12-17 12:32:34 +0530

    button stying

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -4,7 +4,10 @@ import { MdOutlinePublish } from "react-icons/md";
 
 function PublishFormBtn() {
   return (
-    <Button className="gap-2" variant={"outline"}>
+    <Button
+      className="gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400"
+      variant={"outline"}
+    >
       <MdOutlinePublish className="h-4 w-4" />
       Publish
     </Button>
