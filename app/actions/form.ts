2f82096bee75c56c49e9a8405e0ee429b06d8ccb
kartikkpawar <knpwar789@gmail.com>
2023-12-17 12:21:27 +0530

    GetForm By Id Helper method

--- app/actions/form.ts
+++ app/actions/form.ts
@@ -74,3 +74,16 @@ export async function GetForms() {
     },
   });
 }
+export async function GetFormById(id: number) {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.findUnique({
+    where: {
+      userId: user.id,
+      id,
+    },
+  });
+}
