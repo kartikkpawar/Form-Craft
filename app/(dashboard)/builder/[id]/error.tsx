b6c1faf41626c6dc93cf32c22774673a9b40545c
kartikkpawar <knpwar789@gmail.com>
2023-12-17 12:21:44 +0530

    Form Builder Error page

---
+++ app/(dashboard)/builder/[id]/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React, { useEffect } from "react";
+
+function Error({ error }: { error: Error }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className="flex w-full h-full flex-col items-center justify-center gap-4">
+      <h2 className="text-destructive text-4xl"> Something went wrong!</h2>
+      <Button asChild>
+        <Link href={"/"}>Go Back To Home</Link>
+      </Button>
+    </div>
+  );
+}
+
+export default Error;
