38077a5ffe4e80f70f3b24e63b1951ae35b04ea6
kartikkpawar <knpwar789@gmail.com>
2023-12-17 19:32:25 +0530

    Code Refractor and drag events

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,15 +1,12 @@
 "use client";
-import React, { ElementType, useState } from "react";
+import React from "react";
 import DesignerSidebar from "./DesignerSidebar";
 import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
 import useDesigner from "./hooks/useDesigner";
-import {
-  ElementsType,
-  FormElementInstance,
-  FormElements,
-} from "./FormElements";
+import { ElementsType, FormElements } from "./FormElements";
 import { idGenerator } from "@/lib/idGenerator";
+import DesignerElementWrapper from "./DesignerElementWrapper";
 
 function Designer() {
   const droppable = useDroppable({
@@ -47,19 +44,19 @@ function Designer() {
             droppable.isOver && "ring-2 ring-primary/20"
           )}
         >
-          {droppable.isOver ? (
-            <div className="p-4 w-full">
-              <div className="h-[120px] rounded-md bg-primary/20"></div>
-            </div>
-          ) : (
-            elements.length === 0 && (
-              <p className="flex text-3xl text-muted-foreground flex-grow items-center font-bold">
-                Drop Here
-              </p>
-            )
-          )}
+          {droppable.isOver
+            ? elements.length === 0 && (
+                <div className="p-4 w-full">
+                  <div className="h-[120px] rounded-md bg-primary/20"></div>
+                </div>
+              )
+            : elements.length === 0 && (
+                <p className="flex text-3xl text-muted-foreground flex-grow items-center font-bold">
+                  Drop Here
+                </p>
+              )}
           {elements.length > 0 && (
-            <div className="flex flex-col text-background w-full gap-w p-4">
+            <div className="flex flex-col w-full gap-2 p-4">
               {elements.map((element) => (
                 <DesignerElementWrapper key={element.id} element={element} />
               ))}
@@ -70,11 +67,6 @@ function Designer() {
       <DesignerSidebar />
     </div>
   );
-}
-
-function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
-  const DesignerElement = FormElements[element.type].designerComponent;
-  return <DesignerElement />;
 }
 
 export default Designer;
