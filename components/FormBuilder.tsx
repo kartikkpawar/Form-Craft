9a40950540e1105de1ec57f5d845e594f83a6c5d
kartikkpawar <knpwar789@gmail.com>
2023-12-17 13:30:52 +0530

    impoer

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -6,6 +6,7 @@ import SaveFormBtn from "./SaveFormBtn";
 import PublishFormBtn from "./PublishFormBtn";
 import Designer from "./Designer";
 import { DndContext } from "@dnd-kit/core";
+import DragOverlayWrapper from "./DragOverlayWrapper";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
@@ -31,6 +32,7 @@ function FormBuilder({ form }: { form: Form }) {
           <Designer />
         </div>
       </main>
+      <DragOverlayWrapper />
     </DndContext>
   );
 }
