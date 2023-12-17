634ff2ff826a92a6828255dd670a5e7944b563e3
kartikkpawar <knpwar789@gmail.com>
2023-12-17 12:22:31 +0530

    Form builder page

---
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { GetFormById } from "@/app/actions/form";
+import FormBuilder from "@/components/FormBuilder";
+import React from "react";
+
+async function BuilderPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const form = await GetFormById(Number(id));
+
+  if (!form) {
+    throw new Error("Form Not Found");
+  }
+
+  return <FormBuilder form={form} />;
+}
+
+export default BuilderPage;
