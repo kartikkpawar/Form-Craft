11231d401251b6bb912ef807c417d6cd1f14bd5c
kartikkpawar <knpwar789@gmail.com>
2023-12-17 18:31:42 +0530

    Drag and drop  handled

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,7 +1,15 @@
 "use client";
-import React from "react";
+import React, { ElementType, useState } from "react";
 import DesignerSidebar from "./DesignerSidebar";
-import { useDroppable } from "@dnd-kit/core";
+import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
+import { cn } from "@/lib/utils";
+import useDesigner from "./hooks/useDesigner";
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from "./FormElements";
+import { idGenerator } from "@/lib/idGenerator";
 
 function Designer() {
   const droppable = useDroppable({
@@ -11,18 +19,62 @@ function Designer() {
     },
   });
 
+  const { elements, addElement } = useDesigner();
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const isDesignerBtnElement = active?.data?.current?.isDesignerBtnElement;
+      if (isDesignerBtnElement) {
+        const type = active?.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+        addElement(0, newElement);
+      }
+    },
+  });
+
   return (
     <div className="flex w-full h-full">
       <div className="p-4 w-full">
-        <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
-          <p className="flex text-3xl text-muted-foreground flex-grow items-center font-bold">
-            Drop Here
-          </p>
+        <div
+          ref={droppable.setNodeRef}
+          className={cn(
+            "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+            droppable.isOver && "ring-2 ring-primary/20"
+          )}
+        >
+          {droppable.isOver ? (
+            <div className="p-4 w-full">
+              <div className="h-[120px] rounded-md bg-primary/20"></div>
+            </div>
+          ) : (
+            elements.length === 0 && (
+              <p className="flex text-3xl text-muted-foreground flex-grow items-center font-bold">
+                Drop Here
+              </p>
+            )
+          )}
+          {elements.length > 0 && (
+            <div className="flex flex-col text-background w-full gap-w p-4">
+              {elements.map((element) => (
+                <DesignerElementWrapper key={element.id} element={element} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <DesignerSidebar />
     </div>
   );
+}
+
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const DesignerElement = FormElements[element.type].designerComponent;
+  return <DesignerElement />;
 }
 
 export default Designer;
