5bbdf02a4249a3b75e382e86507545b403b8182a
kartikkpawar <knpwar789@gmail.com>
2023-12-18 21:28:45 +0530

    propertiesComponent

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -8,6 +8,21 @@ import {
 import { MdTextFields } from "react-icons/md";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import useDesigner from "../hooks/useDesigner";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Switch } from "../ui/switch";
 
 const type: ElementsType = "TextFiled";
 
@@ -15,15 +30,20 @@ const extraAttributes = {
   label: "Text field",
   helperText: "Helper Text",
   required: false,
-  placholder: "Placeholder value here",
+  placeholder: "Placeholder value here",
 };
+
+const propertiesSchema = z.object({
+  label: z.string().min(2).max(50),
+  helperText: z.string().min(2).max(50),
+  required: z.boolean().default(false),
+  placeholder: z.string().min(2).max(50),
+});
 
 export const TextFiledFormElement: FormElement = {
   type,
   designerComponent: DesignerComponent,
-  propertiesComponent: () => (
-    <div className="text-white">Properties Component</div>
-  ),
+  propertiesComponent: PropertiesComponent,
   formComponent: () => <div className="text-white">Form Component</div>,
   construct: (id: string) => ({
     id,
@@ -40,6 +60,8 @@ type CustomInstance = FormElementInstance & {
   extraAttributes: typeof extraAttributes;
 };
 
+type propertiesFormSchemaType = z.infer<typeof propertiesSchema>;
+
 function DesignerComponent({
   elementInstance,
 }: {
@@ -47,14 +69,14 @@ function DesignerComponent({
 }) {
   const element = elementInstance as CustomInstance;
 
-  const { label, required, placholder, helperText } = element.extraAttributes;
+  const { label, required, placeholder, helperText } = element.extraAttributes;
   return (
     <div className="flex flex-col gap-2 w-full">
       <Label>
         {label}
         {required && "*"}
       </Label>
-      <Input readOnly disabled placeholder={placholder} />
+      <Input readOnly disabled placeholder={placeholder} />
       {helperText && (
         <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
       )}
@@ -62,4 +84,132 @@ function DesignerComponent({
   );
 }
 
-// 2.08.00
+function PropertiesComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { updateElement } = useDesigner();
+
+  const form = useForm<propertiesFormSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    mode: "onBlur",
+    defaultValues: {
+      label: element.extraAttributes.label,
+      helperText: element.extraAttributes.helperText,
+      required: element.extraAttributes.required,
+      placeholder: element.extraAttributes.placeholder,
+    },
+  });
+
+  useEffect(() => {
+    form.reset(element.extraAttributes);
+  }, [element, form]);
+
+  const applyChanges = (values: propertiesFormSchemaType) => {
+    const { label, helperText, required, placeholder } = values;
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        label,
+        helperText,
+        required,
+        placeholder,
+      },
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onBlur={form.handleSubmit(applyChanges)}
+        className="space-y-3"
+        onSubmit={(e) => e.stopPropagation()}
+      >
+        <FormField
+          control={form.control}
+          name="label"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Label</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+              <FormDescription>The placeholder of the filed.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="placeholder"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Placeholder</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+              <FormDescription>The placeholder of the filed.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="helperText"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Helper Text</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                The helperText of the filed. <br /> It will be displayed below
+                field
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="required"
+          render={({ field }) => (
+            <FormItem className="flex items-center justify-between rounded-lg border p-3 shadow-sm">
+              <div className="space-y-0.5">
+                <FormLabel>Required</FormLabel>
+                <FormDescription>
+                  The helperText of the filed. <br /> It will be displayed below
+                  field
+                </FormDescription>
+              </div>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
