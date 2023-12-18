0503ce9f21ce486d2aff9a6a50a9c8c83af81c28
kartikkpawar <knpwar789@gmail.com>
2023-12-18 22:34:35 +0530

    Update form helper

--- app/actions/form.ts
+++ app/actions/form.ts
@@ -87,3 +87,20 @@ export async function GetFormById(id: number) {
     },
   });
 }
+
+export async function UpdateFormContent(id: number, jsonConent: string) {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.update({
+    where: {
+      userId: user.id,
+      id,
+    },
+    data: {
+      content: jsonConent,
+    },
+  });
+}
