c6154cdd983a26d763e59c8a1fc20f6c32d3485a
kartikkpawar <knpwar789@gmail.com>
2023-12-18 22:22:33 +0530

    type change

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -10,7 +10,9 @@ export type FormElement = {
     elementInstance: FormElementInstance;
   }>;
 
-  formComponent: React.FC;
+  formComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
 
   propertiesComponent: React.FC<{
     elementInstance: FormElementInstance;
