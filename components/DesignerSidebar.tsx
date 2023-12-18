c80d5e715852e43dad25e8a003ff6281baf42d98
kartikkpawar <knpwar789@gmail.com>
2023-12-18 21:27:57 +0530

    properties sidebar

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,13 +1,15 @@
 "use client";
 import React from "react";
-import { FormElements } from "./FormElements";
-import SidebarBtnElement from "./SidebarBtnElement";
+import useDesigner from "./hooks/useDesigner";
+import FormElementsSidebar from "./FormElementsSidebar";
+import PropertiesFormSidebar from "./PropertiesFormSidebar";
 
 function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
+
   return (
     <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full">
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextFiled} />
+      {!selectedElement ? <FormElementsSidebar /> : <PropertiesFormSidebar />}
     </aside>
   );
 }
