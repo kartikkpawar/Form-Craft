74ca67ea591f6509e7688b792aa5911508126795
kartikkpawar <knpwar789@gmail.com>
2023-12-20 22:23:27 +0530

    GetFormContentByURL

--- app/submit/[formURL]/page.tsx
+++ app/submit/[formURL]/page.tsx
@@ -1,7 +1,23 @@
+import { GetFormContentByURL } from "@/app/actions/form";
+import { FormElementInstance } from "@/components/FormElements";
+import FormSubmitComponent from "@/components/FormSubmitComponent";
 import React from "react";
 
-function SubmitPage() {
-  return <div>SubmitPage</div>;
+async function SubmitPage({
+  params,
+}: {
+  params: {
+    formURL: string;
+  };
+}) {
+  const form = await GetFormContentByURL(params.formURL);
+  if (!form) {
+    throw new Error("Form Not Found");
+  }
+
+  const formContent = JSON.parse(form.content) as FormElementInstance[];
+
+  return <FormSubmitComponent formURL={params.formURL} content={formContent} />;
 }
 
 export default SubmitPage;
