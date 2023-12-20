dcee0d4383582451183c3091506ab920eeabfe2d
kartikkpawar <knpwar789@gmail.com>
2023-12-20 19:52:02 +0530

    StatsCard made a component

---
+++ components/StatsCard.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
+
+export function StatsCard({
+  title,
+  icon,
+  helperText,
+  loading,
+  className,
+}: {
+  title: string;
+  icon: ReactNode;
+  helperText: string;
+  value: string;
+  loading: boolean;
+  className: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm text-muted-foreground">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
+}
