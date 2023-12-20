4a46b10475a91a3eb2db635bdd8d1aed1772db79
kartikkpawar <knpwar789@gmail.com>
2023-12-20 20:01:21 +0530

    laypout page

--- app/submit/[formURL]/layout.tsx
+++ app/submit/[formURL]/layout.tsx
@@ -1,7 +1,18 @@
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+
 import React, { ReactNode } from "react";
 
-function layout({ children }: { children: ReactNode }) {
-  return <div className="flex w-full flex-grow mx-auto">{children}</div>;
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between border-b border-border h-[60px] px-4 py-2 items-center">
+        <Logo />
+        <ThemeSwitcher />
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
 }
 
-export default layout;
+export default Layout;
