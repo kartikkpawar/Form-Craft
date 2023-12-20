1aa71dd742aecd73ae439d7bca4ff2f0756e5ec9
kartikkpawar <knpwar789@gmail.com>
2023-12-21 00:14:28 +0530

    Auth screen content center

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import { UserButton } from "@clerk/nextjs";
+import React, { ReactNode } from "react";
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between border-b border-border h-[60px] px-4 py-2 items-center">
+        <Logo />
+        <div className="flex gap-4">
+          <ThemeSwitcher />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow h-full items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
