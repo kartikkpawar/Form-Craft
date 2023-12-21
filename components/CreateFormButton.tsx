258f4cc337046307e0563093535929ec5fdcb760
kartikkpawar <knpwar789@gmail.com>
2023-12-21 11:35:21 +0530

    height increased

--- components/CreateFormButton.tsx
+++ components/CreateFormButton.tsx
@@ -60,7 +60,7 @@ function CreateFormButton() {
       <DialogTrigger asChild>
         <Button
           variant={"outline"}
-          className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
+          className="group border border-primary/20 h-[235px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
         >
           <BsFileEarmarkPlus className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
           <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">
